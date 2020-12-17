a7bceec1ea6bea6afc74ebedc2e701ac9a307af5
Xparham <xparham@gmail.com>
2020-12-17 12:09:14 -0500

    added elementIDs to contact form, name, email, subject, and content

---
+++ app2.js
@@ -0,0 +1,6 @@
+const contactForm = document.getElementById('contact-us-form')
+
+const name = document.getElementById('name')
+const email = document.getElementById('email')
+const subject = document.getElementById('subject')
+const content = document.getElementById('content')
